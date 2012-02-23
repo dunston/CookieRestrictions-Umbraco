144855af2e2462731071c7524a46841fe294ef59
dunston <dunston@hackthedot.dk>
2012-02-23 15:21:20 +0100

    Added cookieUtil, to edit cookie settings, in code. Also changed build     location for debug, and release

---
+++ src/CookieRestrictions/CookieUtil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web;
+using CookieRestrictions.Configuration;
+
+namespace CookieRestrictions
+{
+    public static class CookieUtil
+    {
+        public static void AllowCookies()
+        {
+            HttpCookie allowCookie = new HttpCookie(CookieRestrictionsConfig.Instance.CookiesAllowedKey, "on");
+            allowCookie.Expires = DateTime.MaxValue;
+            allowCookie.HttpOnly = false;
+            HttpContext.Current.Response.Cookies.Add(allowCookie);
+        }
+
+
+        public static void DisallowCookies()
+        {
+            HttpCookie httpCookie = HttpContext.Current.Request.Cookies.Get(CookieRestrictionsConfig.Instance.CookiesAllowedKey);
+            if(httpCookie == null)
+                return;
+            httpCookie.Expires = DateTime.Now.AddDays(-1);
+            HttpContext.Current.Response.Cookies.Add(httpCookie);
+        }
+
+        public static bool AllowingCookies()
+        {
+            HttpCookie httpCookie = HttpContext.Current.Request.Cookies.Get(CookieRestrictionsConfig.Instance.CookiesAllowedKey);
+            if (httpCookie == null)
+                return false;
+            return httpCookie.Value == "on" ? true : false;
+        }
+
+    }
+}
